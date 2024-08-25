c3643f33678f93abb1048643157fa49ce25d8e5a
Nabeel Ali <78920962+SyedNabeelAli12@users.noreply.github.com>
2024-08-25 03:01:51 +0200

    check d

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // app.js
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 
 // Middleware as
 app.use(express.json());
 
-
 // Routes Check
-app.get('/', (req, res) => {
-  console.log("API HIT SUCCESS")
-  res.send('Welcome to the Node.js environment example!');
+app.get("/", (req, res) => {
+  console.log("API HIT SUCCESS");
+  res.send("Welcome to the Node.js environment example!");
 });
-
-
 
 // Start server
 app.listen(PORT, () => {
